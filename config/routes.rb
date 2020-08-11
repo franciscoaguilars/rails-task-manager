9f52ab250c30fca76fadb6674b5e53d5df54d9bb
Francisco Aguilar <fiaguilars@gmail.com>
2020-08-11 14:57:20 -0500

    Finished Logic, will add style later.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # List all tasks
+  get 'tasks', to: 'task#index'
+
+  # Routes to create a task
+  get '/task/new', to: "task#new"
+
+  post '/tasks', to: "task#create"
+
+  #Routs to update an instance
+  get '/task/:id/edit', to: "task#edit"
+
+  patch '/task/:id', to: "task#update"
+
+  #Delete
+  delete '/task/:id', to: "task#destroy"
+
+  # Route for one task
+  get '/task/:id', to: 'task#show', as: :task
 end
